0f1ccbd1e7c248ab501be0bd21392d5f1183cbd7
Teemu Puukko <teemuki@gmail.com>
2018-03-24 13:28:00 +0200

    Little more unit testing

--- test/test_cache.py
+++ test/test_cache.py
@@ -7,8 +7,15 @@ from bot.tahmatassubot import Cache
 
 class TestCache(unittest.TestCase):
 
-  def test_equality(self):
-    pass
+  def test_cache(self):
+    cache = Cache({}, 30)
+    self.assertEquals(cache.cache_max_age, 30)    
+
+  def test_expiring_cache(self):
+    cache = Cache({}, 30)
+    self.assertFalse(cache.is_cache_expried())
+    cache.cache_max_age = 0
+    self.assertTrue(cache.is_cache_expried())
 
 if __name__ == '__main__':
   unittest.main()
--- test/test_telegrambot.py
+++ test/test_telegrambot.py
@@ -4,11 +4,19 @@
 import unittest
 
 from bot.tahmatassubot import TelegramBot
+from bot.telegrambot import TelegramBotException
 
 class TestTelegramBot(unittest.TestCase):
 
-  def test_equality(self):
-    pass
+  def test_instantiating_wihtout_token(self):    
+    with self.assertRaises(TelegramBotException) as context:
+      TelegramBot('', bot_name='Tahmatassu')
+      self.assertTrue('Telegram API token missing' in context.exception)
+
+  def test_instantiating_wihtout_bot_name(self):    
+    with self.assertRaises(TelegramBotException) as context:
+      TelegramBot('sdafasd', bot_name='')
+      self.assertTrue("Telegram bot has no name, give a name or use default name" in context.exception)      
 
 if __name__ == '__main__':
   unittest.main()
