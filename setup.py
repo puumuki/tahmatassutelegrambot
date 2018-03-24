66ef095aadb4fe58318db4b988bcb9bcedb5f46c
Teemu Puukko <teemuki@gmail.com>
2018-03-24 14:14:43 +0200

    Little cleaning

--- setup.py
+++ setup.py
@@ -8,13 +8,14 @@ except IOError:
 
 setup(
     name="Tahmatassubot",
-    version="q.0.0",
+    version="1.0.0",
     description="Tahmatassu Telegram Bot",
     license="MIT",
     author="Teemu Puukko",
     packages=find_packages(),
     install_requires=['requests'],
     long_description=long_description,
+    test_suite='runtest',
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.4",
