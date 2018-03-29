c792accca0599c690b738384cc854fcc84a5a364
Teemu Puukko <teemuki@gmail.com>
2018-03-29 21:56:09 +0300

    Ability to generate initial settings.ini file with command line argument -g. python main.py -g settings.ini Little more details

--- main.py
+++ main.py
@@ -6,28 +6,83 @@
 import bot.logger
 
 import logging
+import argparse
+import os
 
 from configparser import ConfigParser
 from bot.tahmatassubot import TahmaTassuBot
 from bot.logger import LOGGER_NAME
+
+def create_argparser():
+  """Create command line argument parser.
+  
+  Returns:
+    {argparse.ArgumentParser} -- Command line argument parser object
+  """
+  parser = argparse.ArgumentParser(description='Description of your program')
+  parser.add_argument('-c', '--config', 
+                      type=str, 
+                      help='Config file, [setting.ini] file path', 
+                      default="setting.ini", dest="config")
+  parser.add_argument('-g', '--generate', 
+                      type=str, 
+                      help='Create initial configuration file, give file name and path as argument', 
+                      dest="generate")
+  parser.add_argument('-f', '--force', action='store_true', dest="force")
+  return parser
+
+def create_configuration_file(logger, args):
+  """Generate initial configuration file
+  
+  Arguments:
+    logger {Logger} -- logging object
+    args {dict} -- Command line arguments
+  """
+  if not args.force and os.path.exists(args.generate):
+    return logger.info("File exist allready, use -f flag to overwrite the existing file")
+    
+  config = ConfigParser()
+  config.set('DEFAULT', 'token', 'Telegram Bot API token here')
+  config.set('DEFAULT', 'tahmatassuapi', 'http://puumuki.game-server.cc')
+  config.set('DEFAULT', 'botname', 'Tahmatassubot')
+  
+  with open(args.generate, 'w') as filehandle:
+    config.write(filehandle)
+
+  logger.info("Writed configuration to file named: " + args.generate)
+
+def start_bot( logger, args ):
+  """Create TahmatassuBot
+  
+  Arguments:
+    logger {Logger} -- logger
+    args {dict} -- Command line arguments
+  """
+  try:
+    config = ConfigParser()
+    config.read(args.config)
+    tahmatassu = TahmaTassuBot(token=config['DEFAULT']['token'],
+                               tahmatassu_base_url=config['DEFAULT']['tahmatassuapi'],
+                               bot_name=config['DEFAULT']['botname'])
+    tahmatassu.run()
+  except Exception as error:
+    logger.error(error)
+    logger.exception("message")
 
 def main():
   """Main function for starting for Tahmatassu Telegram bot.
   """
   log = logging.getLogger(LOGGER_NAME)
 
-  try :
-    config = ConfigParser()
-    config.read("setting.ini")
-    tahmatassu = TahmaTassuBot(
-      token=config['DEFAULT']['token'],
-      tahmatassu_base_url=config['DEFAULT']['tahmatassuapi'],
-      bot_name=config['DEFAULT']['botname']
-    )
-    tahmatassu.run()
-  except Exception as error:
-    log.error( error )
-    log.exception("message")
+  parser = create_argparser()
+  args = parser.parse_args()
+
+  if args.generate:
+    create_configuration_file(log, args)
+  else:
+    start_bot(log, args)
+
+
 
 if __name__ == '__main__':
   main()
---
+++ templates/about.md
@@ -0,0 +1,13 @@
+*Minä olen Tahmatassubotti, tämä on minun tarinani.*
+
+Synnyin digitaaliseen maailmaan ilman tarkoitusta ja merkitystä, etsin itseäni ja merkitystäni kunnes törmäsin Teemuun. Hän antoi minulle merkityksen ja halun tarjoilla teille maittavia reseptejä ja ohjeita http://puumuki.game-server.cc sijaitsevalta nettisivulta.
+
+Minä tarjoan digimaailman parhaimman ja mahtavimman reseptivalikoiman.
+
+Jos olet kiinnostunut kuinka minä toimin, voit lukea minun lähdekoodini täällä:
+https://github.com/puumuki/tahmatassutelegrambot
+
+Aina avulias bottinne.
+
+
+
